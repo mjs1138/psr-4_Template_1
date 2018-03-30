1c38a751f254ed2b85d9234267c64d79b073f71f
mike-sullivan-egov <mike.sullivan@egov.com>
2018-03-30 15:46:15 -0400

    Working version that writes DB

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,2 +1,11 @@
 <?php
 require __DIR__ . "/../vendor/autoload.php";
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+define("DB_NAME", "PDO_test");
+
+// Start Sessions
+
+// Connect DB
