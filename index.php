01726033daad6bf00e0fd7180f5b3bed55306757
mike-sullivan-egov <mike.sullivan@egov.com>
2018-03-30 11:03:19 -0400

    Working version that writes DB

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
 require_once __DIR__ . '/app/bootstrap.php';
 
-use ClassSrcFolder_TopLevelNamespace\Boogie;
+use ClassSrcFolder_TopLevelNamespace\Boogie as Boogie;
 use ClassSrcFolder_TopLevelNamespace\BoogieBoogie;
+
+use ClassSrcFolder_TopLevelNamespace\Database;
 
 $boogie = new Boogie();
 
 $boogieBoogie = new BoogieBoogie();
+
+$database = new Database();
+
+$a=1;
+
+$database->query('INSERT INTO mytable (FName, LName, Age, Gender) VALUES (:fname, :lname, :age, :gender)');
+
+$database->bind(':fname', 'John');
+$database->bind(':lname', 'Smith');
+$database->bind(':age', '24');
+$database->bind(':gender', 'male');
+
+$database->execute();
+
+echo $database->lastInsertId();
